a4da30f85f7cb09aaf2aa3d3592ecd6a6c75ae72
matchi-1 <misheruvynx@gmail.com>
2026-03-22 22:00:44 +0800

    added star assets for bg

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,6 +1,9 @@
 class AppAssets {
-  static const homeLogo = 'assets/images/home/logo.png';
-  static const homeBg = 'assets/images/home/home_bg.png';
-  static const decoGate1 = 'assets/images/home/deco_gate_1.png';
-  static const decoGate2 = 'assets/images/home/deco_gate_2.png';
+  static const homeLogo = 'assets/images/shared/logo.png';
+  static const bgMenu = 'assets/images/shared/background-menu.png';
+  static const star1 = 'assets/images/shared/star1.png';
+  static const star2 = 'assets/images/shared/star2.png';
+  static const star3 = 'assets/images/shared/star3.png';
+  static const star4 = 'assets/images/shared/star4.png';
+
 }
